6e4efebdb45d83e7720371cba9a95d7e5048c822
caozhihui <caozhihui_1998>
2022-08-01 17:47:19 +0800

    新增11种语言

--- jsonToXls.js
+++ jsonToXls.js
@@ -8,11 +8,20 @@ fs.readFile('./lib/name.json', 'utf8', (err, data) => {
 
     Object.keys(json).forEach(item => {
         const temp = {
-            'module': '行情模块',
+            'module': 'xx模块',
             'key': item,
             'zhcn': json[item],
             'enww': '',
-            'kokr': ''
+            'kokr': '',
+            'Filipino': '',
+            'Thai': '',
+            'Spanish': '',
+            'Japanese': '',
+            'Vietnamese': '',
+            'Turkish': '',
+            'Portuguese': '',
+            'FRENCH': '',
+            'Arabic': ''
         }
         jsonArray.push(temp);
     });
--- xlsToJson.js
+++ xlsToJson.js
@@ -5,15 +5,45 @@ const xlsx = require('node-xlsx');
 /** 解析excel文档 */
 const sheets = xlsx.parse('./lib/name.xlsx');
 const sheet = sheets[0]
-const cn = {}
-const en = {}
-const kor = {}
+
+const zhcn = {}
+const enww = {}
+const kokr = {}
+const Filipino = {}
+const tai_land = {}
+const Spanish = {}
+const Japanese = {}
+const Vietnamese = {}
+const Turkish = {}
+const Portuguese = {}
+const FRENCH = {}
+const Arabic = {}
+
 sheet.data.forEach((item, index) => {
-    cn[item[1]] = item[2]
-    en[item[1]] = item[3]
-    kor[item[1]] = item[4]
+    zhcn[item[1]] = item[2]
+    enww[item[1]] = item[3]
+    kokr[item[1]] = item[4]
+    Filipino[item[1]] = item[5]
+    tai_land[item[1]] = item[6]
+    Spanish[item[1]] = item[7]
+    Japanese[item[1]] = item[8]
+    Vietnamese[item[1]] = item[9]
+    Turkish[item[1]] = item[10]
+    Portuguese[item[1]] = item[11]
+    FRENCH[item[1]] = item[12]
+    Arabic[item[1]] = item[13]
 })
 
-fs.writeFileSync('./dist/en.json', JSON.stringify(en))
-fs.writeFileSync('./dist/kor.json', JSON.stringify(kor))
-fs.writeFileSync('./dist/cn.json', JSON.stringify(cn))
+fs.writeFileSync('./dist/zhcn.json', JSON.stringify(zhcn))
+fs.writeFileSync('./dist/enww.json', JSON.stringify(enww))
+fs.writeFileSync('./dist/kokr.json', JSON.stringify(kokr))
+fs.writeFileSync('./dist/Filipino.json', JSON.stringify(Filipino))
+fs.writeFileSync('./dist/tai_land.json', JSON.stringify(tai_land))
+fs.writeFileSync('./dist/Spanish.json', JSON.stringify(Spanish))
+fs.writeFileSync('./dist/Japanese.json', JSON.stringify(Japanese))
+fs.writeFileSync('./dist/Vietnamese.json', JSON.stringify(Vietnamese))
+fs.writeFileSync('./dist/Turkish.json', JSON.stringify(Turkish))
+fs.writeFileSync('./dist/Portuguese.json', JSON.stringify(Portuguese))
+fs.writeFileSync('./dist/FRENCH.json', JSON.stringify(FRENCH))
+fs.writeFileSync('./dist/Arabic.json', JSON.stringify(Arabic))
+
